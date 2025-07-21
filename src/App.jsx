f5be7a80edbe96640ad046fcb558d44395a4461d
Yin Yu <yinyu.21.tan@gmail.com>
2025-07-21 17:27:16 +0800

    TechStack
    techstack page with icons added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 import LogoSection from "./sections/LogoSection.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
         <LogoSection />
         <FeatureCards />
         <ExperienceSection />
+        <TechStack />
     </>
 )
 }
