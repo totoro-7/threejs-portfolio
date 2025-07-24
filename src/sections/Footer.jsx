fcabe9c44531b4e3c39994ac9793925e42cf6b55
Tan Yin Yu <143823075+totoro-7@users.noreply.github.com>
2025-07-24 17:58:14 +0800

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-container">
                 <div className="flex flex-col justify-center md:items-start items-center">
-                    <a>work-in-progress</a>
+{/*                     <a>work-in-progress</a> */}
                 </div>
                 <div className="socials">
                     {socialImgs.map((img) => (
